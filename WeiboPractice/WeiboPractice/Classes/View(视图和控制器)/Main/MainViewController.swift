2cebd199ecbb29d16d9fee13aa9b1320a8bbb076
HaydenLiu <huangxunliu@gmail.com>
2019-11-20 16:14:31 +1100

    MVVC架构

---
+++ WeiboPractice/WeiboPractice/Classes/View(视图和控制器)/Main/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  WeiboPractice
+//
+//  Created by 刘皇逊 on 20/11/19.
+//  Copyright © 2019 hayden. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
