24d02dc3d4db63c313c4fe30ae6429388c37102b
HaydenLiu <huangxunliu@gmail.com>
2019-11-20 12:24:30 +1100

    commit version1.0

---
+++ WeiboPractice/WeiboPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeiboPractice
+//
+//  Created by 刘皇逊 on 20/11/19.
+//  Copyright © 2019 hayden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
