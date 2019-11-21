8d17371eb3da86c029c72646825f48eb2c26b851
HaydenLiu <huangxunliu@gmail.com>
2019-11-21 14:45:40 +1100

    验证访客

--- WeiboPractice/WeiboPractice/Classes/View(视图和控制器)/Main/MainViewController.swift
+++ WeiboPractice/WeiboPractice/Classes/View(视图和控制器)/Main/MainViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class MainViewController: UITabBarController {
 
+    //MARK: -监听方法
+    ///点击撰写按钮
+    ///如果单纯使用·private·循环无法正确发送消息
+    ///@objc可以保证运行循环发送此消息，即使这个函数被标记为private
+    @objc private func clickComposedButton(){
+        print("click me!")
+    }
+    //MARK: -视图生命周期函数
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        //添加控制器，并不会创建tabbar里的按钮
+        //懒加载，所有控件都是延迟创建的
         addChildViewControllers()
         setupComposedButton()
         // Do any additional setup after loading the view.
@@ -45,14 +54,17 @@ class MainViewController: UITabBarController {
 extension MainViewController {
     //设置撰写按钮
     private func setupComposedButton(){
+        //1.添加按钮
         tabBar.addSubview(composedButton)
         
-        //调整按钮
+        //2.调整按钮
         let count = children.count
         //让按钮宽一点
         let w = tabBar.bounds.width/CGFloat(count) - 1
         
         composedButton.frame = tabBar.bounds.insetBy(dx: 2*w, dy: -10)
+        //3.添加监听方法
+        composedButton.addTarget(self, action: #selector(clickComposedButton), for: .touchUpInside)
     }
 
     //添加所有控制器
---
+++ WeiboPractice/WeiboPractice/Classes/View(视图和控制器)/Main/Visitor/VisitorTableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  VisitorTableViewController.swift
+//  WeiboPractice
+//
+//  Created by 刘皇逊 on 21/11/19.
+//  Copyright © 2019 hayden. All rights reserved.
+//
+
+import UIKit
+
+class VisitorTableViewController: UITableViewController {
+
+    private var userLogon = false
+    
+    override func loadView() {
+        print("come here")
+        super.loadView()
+        //替换根视图
+//        view = UIView()
+//        view.backgroundColor = UIColor.orange
+    }
+
+}
